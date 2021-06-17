72a3f994d9e7fd5e51c2d8a13f30fed6690425e0
Souvik Das <sd8338820@gmail.com>
2021-06-17 11:20:22 +0530

    update

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function popup() {
+function showpopup() {
     var popup = document.getElementById("myPopup");
     popup.classList.add("show");
     setTimeout(hide, 2000);
 
 }
-function hide() {
+function hidepopup() {
     var popup = document.getElementById("myPopup");
     popup.classList.remove("show");
 
